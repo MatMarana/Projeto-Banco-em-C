d47763d1e4775da3d5a3f583497d351feb437819
mateus marana <assumateus@gmail.com>
2024-10-19 21:29:44 -0300

    feture[verificaCliente]

--- Library.c
+++ Library.c
@@ -4,6 +4,16 @@
 
 Dados_Pessoas dados[1000];
 
+int verificaCliente(int numeroClientes, char cpf[15], int senha, int indice){
+  if(strcmp(dados[indice].cpf,cpf) == 0){
+    if(dados[indice].senha == senha){
+        return 1;
+    }
+    printf("Senha incorreta\n");
+  }
+  return 0;
+}
+
 void cadastraUsuario(int numeroClientes){ // numero de clientes é usado para controlar a posição dos indices do array de structs
   printf("Digite o nome do usuario: "); 
   scanf("%s", dados[numeroClientes].nome); // Recebe o nome de usuario digitado e passa para o array de strcut
@@ -40,17 +50,16 @@ void apagarCliente(int numeroClientes,char cpf[15]){
           break;
       }
   }
-
   if (i == numeroClientes) { // CPF não encontrado
       printf("Digite um CPF valido\n");
       printf("\n");
       return;
   }
-
-  for (; i < numeroClientes - 1; i++) { // apaga o clietente com o cpf igual ao indice i
+  for (; i < numeroClientes - 1; i++) { // apaga o clietente com o cpf iguial a variavel i
       dados[i] = dados[i + 1];
   }
 }
+
 void listaCliente(int numeroClientes){
   for(int i = 0; i < numeroClientes; i++){  // identa o array de structs e imprime as informações dos usuarios
     printf("Nome: %s\n", dados[i].nome); // imprime o nome
@@ -64,93 +73,89 @@ void listaCliente(int numeroClientes){
 void deposito(int numeroClientes, char cpf[15]){ 
   double valorDeposito;
   for(int i =0; i < numeroClientes; i ++){ // identa o array de structs ate o numero de pessoas existentes
-    if (strcmp(cpf, dados[i].cpf) == 0){  //compara os cpfs do array de structs com o digitado
+    if (strcmp(dados[i].cpf,cpf) == 0){  //compara os cpfs do array de structs com o digitado
       printf("Digite o valor a ser depositado: "); // recebe o valor depositado
       scanf("%lf", &valorDeposito);
       dados[i].operacoes->deposito= valorDeposito; // armazena o valor depositado na struct operações
       dados[i].valor_incial = dados[i].valor_incial + valorDeposito; // altera o saldo na conta
       break;
     } 
-    printf("Digite um cpf existente\n");
   }
 }
 
 void debito(int numeroClientes, char cpf[15], int senha){ 
   double valorDigitado;
   double valorDebito;
-  for(int i =0; i < numeroClientes; i ++){
-    if (strcmp(cpf, dados[i].cpf) == 0){ //compara os cpfs do array de structs com o digitado
+  for(int i = 0; i < numeroClientes; i++){
+    if (verificaCliente(numeroClientes,cpf,senha,i) == 1){ //compara os cpfs do array de structs com o digitado
       if (strcmp(dados[i].tipo_conta, "Comum") == 0){ // se a conta for comum
-        if(senha == dados[i].senha){ // se a senha estiver correta
-          if(dados[i].valor_incial > -1000){ // caso o saldo for menor que -1000 
-            printf("Digite o valor a ser debitado: "); // recebe o valor debitado
-            scanf("%lf", &valorDigitado);
-            valorDebito = valorDigitado + (valorDigitado * 0.05); // calcula o valor debitado mais 5% de taxa
-            dados[i].operacoes->debito = valorDebito; //armazena o valor debitado na struct de operações
-            dados[i].valor_incial = dados[i].valor_incial - valorDebito; // altera o saldo na conta
-            break;
-          } else {
-            printf("Saldo Insuficinete\n"); // caso o saldo for menor que -1000 não deixa fazer a operação
-            printf("\n");
-            break;
-          }
-        } 
-        printf("Digite a senha correta");
+        if(dados[i].valor_incial > -1000){ // caso o saldo for menor que -1000 
+          printf("Digite o valor a ser debitado: "); // recebe o valor debitado
+          scanf("%lf", &valorDigitado);
+          valorDebito = valorDigitado + (valorDigitado * 0.05); // calcula o valor debitado mais 5% de taxa
+          dados[i].operacoes->debito = valorDebito; //armazena o valor debitado na struct de operações
+          dados[i].valor_incial = dados[i].valor_incial - valorDebito; // altera o saldo na conta
+          break;
+        } else {
+          printf("Saldo Insuficinete\n"); // caso o saldo for menor que -1000 não deixa fazer a operação
+          printf("\n");
+          break;
+        }
       } else { // se a conta for premium
-        if(senha == dados[i].senha){ // se a senha estiver correta
-          if(dados[i].valor_incial > -5000){ // caso o saldo for menor que -5000 
-            printf("Digite o valor a ser debitado: "); // recebe o valor debitado
-            scanf("%lf", &valorDigitado);
-            valorDebito = valorDigitado + (valorDigitado * 0.03); // calcula o valor debitado mais 3% de taxa
-            dados[i].operacoes->debito = valorDebito; //armazena o valor debitado na struct de operações
-            dados[i].valor_incial = dados[i].valor_incial - valorDebito; // altera o saldo na conta
-            break;
-          } else {
-            printf("Saldo Insuficinete\n"); // caso o saldo for menor que -5000 não deixa fazer a operação
-            printf("\n");
-            break;
-          }
-        } 
-      }
-    } 
-  }
+        if(dados[i].valor_incial > -5000){ // caso o saldo for menor que -5000 
+          printf("Digite o valor a ser debitado: "); // recebe o valor debitado
+          scanf("%lf", &valorDigitado);
+          valorDebito = valorDigitado + (valorDigitado * 0.03); // calcula o valor debitado mais 3% de taxa
+          dados[i].operacoes->debito = valorDebito; //armazena o valor debitado na struct de operações
+          dados[i].valor_incial = dados[i].valor_incial - valorDebito; // altera o saldo na conta
+          break;
+        } else {
+          printf("Saldo Insuficinete\n"); // caso o saldo for menor que -5000 não deixa fazer a operação
+          printf("\n");
+          break;
+        }
+      } 
+    }
+    printf("TENTE NOVAMENTE\n");
+  } 
 }
 
 void transferencia(int numeroClientes, char cpf[15], int senha){ 
   char cpfRecebe[15];
   double valorEnviado;
   for(int i = 0; i < numeroClientes; i ++){ 
-    if(strcmp(dados[i].cpf, cpf ) == 0){ //compara os cpfs do array de structs com o digitado
-        if(senha == dados[i].senha){
-          printf("Digite um valor a ser transferido: ");// recebe o valor transferido
-          scanf("%lf", &valorEnviado);
-          if (strcmp(dados[i].tipo_conta, "Comum") == 0){
-            if(dados[i].valor_incial > -1000){ // caso o saldo for menor que -1000 
-              dados[i].valor_incial = dados[i].valor_incial - valorEnviado; // altera o saldo na conta
-              dados[i].operacoes->transferencia = valorEnviado; //armazena o valor tranferido na struct de operações
-            } else {
-              printf("Saldo Insuficiente\n"); // caso o saldo for menor que -1000 não deixa fazer a operação
-              printf("\n");
-              break;
-            } 
-          } else { // se a conta for premium 
-            if(dados[i].valor_incial > -5000){  // caso o saldo for menor que -5000 
-              dados[i].valor_incial = dados[i].valor_incial - valorEnviado; // altera o saldo na conta
-            } else {
-              printf("Saldo Insuficinete\n"); // caso o saldo for menor que -5000 não deixa fazer a operação
-              printf("\n");
-              break;
-            }
+    if(verificaCliente(numeroClientes, cpf, senha, i) == 1){ //compara os cpfs do array de structs com o digitado
+        printf("Digite um valor a ser transferido: ");// recebe o valor transferido
+        scanf("%lf", &valorEnviado);
+        if (strcmp(dados[i].tipo_conta, "Comum") == 0){
+          if(dados[i].valor_incial > -1000){ // caso o saldo for menor que -1000 
+            dados[i].valor_incial = dados[i].valor_incial - valorEnviado; // altera o saldo na conta
+            dados[i].operacoes->transferencia = valorEnviado; //armazena o valor tranferido na struct de operações
+          } else {
+            printf("Saldo Insuficiente\n"); // caso o saldo for menor que -1000 não deixa fazer a operação
+            printf("\n");
+            break;
           } 
-          printf("Digite o CPF da conta que vai receber o dinheiro: ");  // recebe o cpf que vai receber a transferencia
-          scanf("%s", cpfRecebe); 
-          for(int i = 0; i < numeroClientes; i++){
-            if(strcmp(cpfRecebe, dados[i].cpf) == 0){ // se o cpf existe
-              dados[i].valor_incial = dados[i].valor_incial + valorEnviado; // altera o saldo na conta 
-            }
+        } else { // se a conta for premium 
+          if(dados[i].valor_incial > -5000){  // caso o saldo for menor que -5000 
+            dados[i].valor_incial = dados[i].valor_incial - valorEnviado; // altera o saldo na conta
+          } else {
+            printf("Saldo Insuficinete\n"); // caso o saldo for menor que -5000 não deixa fazer a operação
+            printf("\n");
+            break;
+          }
+        } 
+      
+        printf("Digite o CPF da conta que vai receber o dinheiro: ");  // recebe o cpf que vai receber a transferencia
+        scanf("%s", cpfRecebe); 
+        for(int i = 0; i < numeroClientes; i++){
+          if(strcmp(cpfRecebe, dados[i].cpf) == 0){ // se o cpf existe
+            dados[i].valor_incial = dados[i].valor_incial + valorEnviado; // altera o saldo na conta 
           }
         }
-      }   
+      
+      } 
+    printf("TENTE NOVAMENTE\n");
     }
   }
 
--- Library.h
+++ Library.h
@@ -2,29 +2,24 @@
 #define LIBRARY_H_INCLUDED
 
 typedef struct Dados_Conta{
-
     double deposito;
     double debito;
     double transferencia;
-
 }Dados_Conta;
 
 typedef struct Dados_Pessoas{
-
     int senha;
     char tipo_conta[10];
     double valor_incial;
     char nome[100];
     char cpf[15];
     Dados_Conta operacoes[100];
-    
 }Dados_Pessoas;
-
-
 
 void apagarCliente(int numeroClientes, char cpf[15]);
 void listaCliente(int numeroClientes);
 void tipoConta(int numeroClientes, int escolhaConta);
+int verificaCliente(int numeroClientes, char cpf[15], int senha, int indice);
 void cadastraUsuario(int numeroClientes);
 void deposito(int numeroClientes, char cpf[15]);
 void debito(int numeroClientes, char cpf[15], int senha);
@@ -32,4 +27,5 @@ void escrever(int quantidade_tarefa);
 void ler(int numeroClientes);
 void transferencia(int numeroClientes, char cpf[15], int senha);
 void extrato(int num_elementos, char cpf[15], int senha);
-#endif // LIBRARY_H_INCLUDED
+
+#endif
